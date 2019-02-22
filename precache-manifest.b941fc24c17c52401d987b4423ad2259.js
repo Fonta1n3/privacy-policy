87b4bef54e7750ac364744a11634f8b2b6835601
FontaineDenton <tripkeyapp@gmail.com>
2019-02-22 12:48:43 +0800

    Updates

---
+++ precache-manifest.b941fc24c17c52401d987b4423ad2259.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5924b57800e962422327",
+    "url": "/privacy-policy/static/js/main.5924b578.chunk.js"
+  },
+  {
+    "revision": "0b0ccfdbeb44e3f5f18c",
+    "url": "/privacy-policy/static/js/runtime~main.0b0ccfdb.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/privacy-policy/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "9c76ceccf6cc0ed2c24afffdee6fb08e",
+    "url": "/privacy-policy/index.html"
+  }
+];
